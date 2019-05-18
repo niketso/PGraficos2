526a5bb3f655005827818ab3810787e530f04b13
nicolas <nicolaspiccitto@gmail.com>
2019-05-17 21:51:14 -0300

    PG2-v0.53
     Class MeshImporter updated

--- Engine/EngineDLL/ModelImporter.cpp
+++ Engine/EngineDLL/ModelImporter.cpp
@@ -11,18 +11,80 @@ ModelImporter::~ModelImporter()
 {
 }
 
-MeshData ModelImporter::LoadMesh(const char * MeshName)
+bool ModelImporter::LoadMesh(const char* MeshName)
 {
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	return MeshData();
+	Clear(m_Textures);
+
+	bool Ret = false;
+	Assimp::Importer Importer;
+
+	const aiScene* pScene = Importer.ReadFile(MeshName, ASSIMP_LOAD_FLAGS);
+
+	if (pScene) {
+		Ret = InitFromScene(pScene, MeshName);
+	}
+	else {
+		printf("Error parsing '%s': '%s'\n", MeshName, Importer.GetErrorString());
+	}
+
+	return Ret;
 }
+
+bool ModelImporter::InitFromScene(const aiScene * pScene, const std::string & Filename)
+{
+	m_Entries.resize(pScene->mNumMeshes);
+	m_Textures.resize(pScene->mNumMaterials);
+
+	// Initialize the meshes in the scene one by one
+	for (unsigned int i = 0; i < m_Entries.size(); i++) {
+		const aiMesh* paiMesh = pScene->mMeshes[i];
+		InitMesh(i, paiMesh);
+	}
+	return false;
+}
+
+void ModelImporter::InitMesh(unsigned int Index, const aiMesh * paiMesh)
+{
+	m_Entries[Index].MaterialIndex = paiMesh->mMaterialIndex;
+
+	std::vector<Vertex> Vertices;
+	std::vector<unsigned int> Indices;
+
+	const aiVector3D Zero3D(0.0f, 0.0f, 0.0f);
+
+	for (unsigned int i = 0; i < paiMesh->mNumVertices; i++) {
+		const aiVector3D* pPos = &(paiMesh->mVertices[i]);
+		const aiVector3D* pNormal = &(paiMesh->mNormals[i]);
+		const aiVector3D* pTexCoord = paiMesh->HasTextureCoords(0) ? &(paiMesh->mTextureCoords[0][i]) : &Zero3D;
+
+		Vertex v(vec3(pPos->x, pPos->y, pPos->z),
+			vec2(pTexCoord->x, pTexCoord->y),
+			vec3(pNormal->x, pNormal->y, pNormal->z));
+
+		Vertices.push_back(v);
+	}
+
+	for (unsigned int i = 0; i < paiMesh->mNumFaces; i++) {
+		const aiFace& Face = paiMesh->mFaces[i];
+		assert(Face.mNumIndices == 3);
+		Indices.push_back(Face.mIndices[0]);
+		Indices.push_back(Face.mIndices[1]);
+		Indices.push_back(Face.mIndices[2]);
+	}
+
+	m_Entries[Index].Init(Vertices, Indices);
+}
+
+void ModelImporter::Clear(vector<BMPheader*>& texture)
+{
+	for (unsigned int i = 0; i < m_Textures.size(); i++) {
+		if (texture[i] != NULL)
+		{
+			delete texture[i];
+			texture[i] = NULL;
+		}
+	}
+}
+
 
 
--- Engine/EngineDLL/ModelImporter.h
+++ Engine/EngineDLL/ModelImporter.h
@@ -1,9 +1,16 @@
 #pragma once
+#define ASSIMP_LOAD_FLAGS (aiProcess_Triangulate | aiProcess_GenSmoothNormals | aiProcess_FlipUVs | aiProcess_JoinIdenticalVertices)
 #include "Exports.h"
 #include <vector>
-
-//#include"../assimp/Importer.hpp"
+#include"Importer.h"
+#include<assimp/Importer.hpp>
+#include<assimp/postprocess.h>
+#include<assimp/scene.h>
+#include<gl/glew.h>
+#include<glm/vec2.hpp>
+#include<glm/vec3.hpp>
 using namespace std;
+using namespace glm;
 
 struct MeshData
 {	
@@ -12,14 +19,53 @@ struct MeshData
 	vector<float> uvVec;
 };
 
+struct Vertex
+{
+	vec3 m_pos;
+	vec2 m_tex;
+	vec3 m_normal;
+
+	Vertex() {}
+
+	Vertex(const vec3& pos, const vec2& tex, const vec3& normal)
+	{
+		m_pos = pos;
+		m_tex = tex;
+		m_normal = normal;
+	}
+};
+struct MeshEntry {
+	MeshEntry() {};
+
+	~MeshEntry() {};
+
+	void Init(const std::vector<Vertex>& Vertices,
+		const std::vector<unsigned int>& Indices);
+
+	GLuint VB;
+	GLuint IB;
+	unsigned int NumIndices;
+	unsigned int MaterialIndex;
+};
+
+std::vector<MeshEntry> m_Entries;
+std::vector<BMPheader*> m_Textures;
+
+
+
 class ENGINEDLL_API ModelImporter
 {
 private:
 vector<MeshData> aux;
+
+
 public:
 	ModelImporter();
 	~ModelImporter();
-	static MeshData LoadMesh(const char * MeshName);
+	bool LoadMesh(const char * MeshName);
+	bool InitFromScene(const aiScene* pScene, const std::string& Filename);
+    void InitMesh(unsigned int Index, const aiMesh* paiMesh);
+    void Clear(vector<BMPheader*> &texture);
 
 };
 
